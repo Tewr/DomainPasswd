4be443381b4c68a7d9b2ea22b6478fa085c6442f
Tor KNUTSSON <t.knutsson@lombardodier.com>
2018-08-09 09:28:46 +0200

    Renamed to DomainPasswd not to conflict with bash/powershell command

--- Passwd/Properties/AssemblyInfo.cs
+++ Passwd/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Passwd")]
-[assembly: AssemblyDescription("Changes the password of a domain user. Do not use this application for computer accounts, use NET USER instead.")]
-[assembly: AssemblyProduct("Passwd")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
+[assembly: AssemblyTitle("DomainPasswd")]
+[assembly: AssemblyDescription("Changes the password of a domain user other than the user durrently logged on. Do not use this application for computer accounts, use NET USER instead.")]
+[assembly: AssemblyProduct("DomainPasswd")]
+[assembly: AssemblyCopyright("Copyright © 2016 https://github.com/Tewr/DomainPasswd")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -25,5 +25,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyCompany("")]
+
